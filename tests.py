5b7b2dcfcc35616a4ebc1af61f914762c4a7d880
lfisher1998 <46456099+lfisher1998@users.noreply.github.com>
2019-02-27 15:57:42 -0500

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,138 @@
+import datetime
+import io
+import sys
+import unittest
+from unittest.mock import patch
+
+from peewee import SqliteDatabase
+
+import work_log
+
+
+test_db = SqliteDatabase(':memory:', )
+
+MODELS = [work_log.Database]
+
+
+class WorkLogTests(unittest.TestCase):
+    def setUp(self):
+        # Bind model classes to test db. Since we have a complete list of
+        # all models, we do not need to recursively bind dependencies.
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+
+        test_db.connect(reuse_if_open=True)
+        test_db.create_tables(MODELS)
+        
+        work_log.Database.create(employee_field="Chris", name="Task Name", time=22, notes="Notes")
+        
+    
+    def test_invalid_time_input(self):
+        with patch('builtins.input', side_effect=['','100']):
+            result = work_log.get_valid_time()
+        self.assertEqual(result, 100)
+        
+        
+    def test_add_entry(self):
+        work_log.add_entry(employee="Dave", name="Task", time=26, notes="Lots of notes")
+        entry = work_log.Database(employee="Dave", time=26)
+        self.assertEqual(entry.employee, "Dave")
+        
+    
+    def test_all_dates_(self):
+        now = datetime.datetime.now()
+        date_str = now.strftime('%Y-%m-%d')
+        result = work_log.all_dates()
+        self.assertIn(date_str, result)
+        
+        
+    def test_all_employees(self):
+        user_input = work_log.Database.employee_field
+        result = work_log.all_employees()
+        self.assertIn(user_input, result)
+        
+        
+    def test_search_by_time(self):
+        test_input = 22
+        result = work_log.search_by_time(test_input)
+        self.assertEqual(len(result), 1)
+        
+        
+    def test_time_query(self):
+        test_input = 22
+        result = work_log.time_query(test_input)
+        self.assertEqual(len(result), 1) 
+        
+        
+    def test_search_by_date(self):
+        test_input = '2019-02-23'
+        result = work_log.search_by_date(test_input)
+        self.assertEqual(len(result), 0)
+        
+        
+    def test_date_query(self):
+        test_input = '2019-02-23'
+        result = work_log.date_query(test_input)
+        self.assertEqual(len(result), 0)
+        
+        
+    def test_search_by_exact(self):
+        test_input = 'Task'
+        result = work_log.search_by_exact(test_input)
+        self.assertEqual(len(result), 1)
+        
+        
+    def test_exact_query(self):
+        test_input = 'Task'
+        result = work_log.exact_query(test_input)
+        self.assertEqual(len(result), 1)
+        
+        
+    def test_search_by_employee(self):
+        test_input = 'Chris'
+        result = work_log.search_by_employee(test_input)
+        self.assertEqual(len(result), 1)
+        
+        
+    def test_employee_query(self):
+        test_input = 'Chris'
+        result = work_log.employee_query(test_input)
+        self.assertEqual(len(result), 1)  
+        
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_search_entries_menu_print(self, mock_stdout):
+        result = work_log.search_entries_menu()
+        self.assertNotEqual(mock_stdout.getvalue(), "These are your options for searching:\n"
+          "1. Search by Date\n"
+          "2. Search by the Time Spent on task\n"
+          "3. Search by Exact Search\n"
+          "4. Search by Employee\n"
+          "5. Return to Main Menu\n")
+        
+        
+    def test_display_results_false(self):
+        result = False
+        results = work_log.display_results(result)
+        self.assertEquals(result, 0)
+        
+    
+#    def test_display_results_true(self):
+#        results = work_log.display_results(result)
+#        self.assertIsInstance(mock_stdout.getvalue(), "Employee Name: {}\n"
+#                  "Task Name: {}\n"
+#                  "Task Time: {}\n"
+#                  "Task Notes: {}\n"
+#                  "Task Date: {}\n\n")
+        
+    
+    def tearDown(self):
+        # Not strictly necessary since SQLite in-memory databases only live
+        # for the duration of the connection, and in the next step we close
+        # the connection...but a good practice all the same.
+        test_db.drop_tables(MODELS)
+
+        # Close connection to db.
+        test_db.close()
+    
+    
+if __name__ == '__main__':
+    unittest.main()
---
+++ work_log.py
@@ -0,0 +1,262 @@
+import os
+import datetime
+
+
+from peewee import *
+
+
+intro_message = "Welcome to the Database Work Log!"
+
+db = SqliteDatabase('entry.db')
+
+class Database(Model):
+    employee_field = CharField(max_length=100)
+    name = CharField(max_length=100)
+    time = IntegerField(default=0)
+    date = DateField(default=datetime.datetime.now)
+    notes = TextField()
+    
+    
+    class Meta:
+        database = db    
+        
+# Verified with Chris I could opt out of testing this.
+def create_database(): # pragma: no cover 
+    db.connect()
+    db.create_tables([Database], safe=True)
+    
+
+def clear():
+    """Clear the screen"""
+    os.system('cls' if os.name == 'nt' else 'clear')
+    return None
+
+
+def get_valid_time():
+    while True:
+        try:
+            time = int(input("Please enter an integer value for how many minutes were worked on this task: "))
+            clear()
+            break
+        except ValueError:
+            clear()
+            print("Please enter a number value! (Ex: 25, 12): ")
+    return time
+
+
+def main(): 
+    #loop until the user decides to quit the program
+    while True:
+        # Display a series of choices so the user knows their option.
+        print("Here are your options:")
+        print("1. Add an entry")
+        print("2. Look up previous entry")
+        print("3. Quit the program")
+        # Ask that user for their option.
+        # Validate that option.
+        try:
+            choice = int(input("Please enter a number for your choice: "))
+            if choice < 1 or choice > 3:
+                clear()
+                # if not valid: Print an appropriate message.
+                print("Please enter a 1, 2, or 3")
+            elif choice == 1:
+                clear()
+                # Add Entry
+                employee_field = input("Please enter an employee name for this task: ")
+                name = input("Please enter a task name: ")
+                time = get_valid_time()
+                notes = input("Please enter any notes for this task (optional): ")
+                entry = add_entry(employee_field, name, time, notes)
+                
+                print("Entry: {} added!".format(entry.name))
+            elif choice == 2:
+                search_entries_menu()
+                choice2 = search_entries_input()
+                
+                if choice2 == 1:
+                    clear()
+                    print("Here are all the dates that have entries included: ")
+                    for dates in all_dates():
+                        print(dates)
+                    user_input = input("Please enter a date in YYYY-MM-DD format: ")
+                    result = search_by_date(user_input)
+                elif choice2 == 2:
+                    while True:
+                        try:
+                            user_input = int(input("Please enter an integer of minutes worked on task: "))
+                            break
+                        except ValueError:
+                            clear()
+                            print("Enter an integer!")
+                    clear()
+                    result = search_by_time(user_input)
+                elif choice2 == 3:
+                    while True:
+                        user_input = input("Please type a phrase to search: ")
+                        if user_input:
+                            break
+                            clear()
+                        elif user_input.strip() == '':
+                            clear()
+                            print("You didn't type anything!! ")
+                            user_input = None
+                    result = search_by_exact(user_input)
+                elif choice2 == 4:
+                    print("Here are all the employees who have entries: ")
+                    for name in all_employees():
+                        print(name)
+                    user_input = input("Please enter the name of the employee you'd like to see entries about: ")
+                    result = search_by_employee(user_input)                
+                clear()
+                display_results(result)                
+            elif choice == 3:
+                break
+        except ValueError:
+            clear()
+            # if not valid: Print an appropriate message.
+            print("Please enter a number as an option!")
+        
+        
+def add_entry(employee, name, time, notes):
+    return Database.create(employee_field=employee, name=name, time=time, notes=notes)
+        
+        
+def search_entries_menu():
+    clear()
+    # when the user wants to search for an entry, these are the options displayed
+    print("These are your options for searching:\n"
+          "1. Search by Date\n"
+          "2. Search by the Time Spent on task\n"
+          "3. Search by Exact Search\n"
+          "4. Search by Employee\n"
+          "5. Return to Main Menu\n"
+          )
+    return None
+
+
+def search_entries_input():
+    # ask for user input
+    while True:
+        try:
+            search_choice = int(input("Please type your decision with its corresponding number (Ex. 1, 2, 3): "))
+            if search_choice > 5 or search_choice < 1:
+                clear()
+                search_choice = None
+                search_entries_menu()
+                print("Please enter a number between and including 1-5!")
+            # Search by date option
+            elif search_choice == 1:
+                break
+            # Search by time option
+            elif search_choice == 2:
+                break
+            #Search by exact match option
+            elif search_choice == 3:
+                break
+            # Search by exact pattern or expression
+            elif search_choice == 4:
+                break 
+            # Go back to main menu option
+            elif search_choice == 5:
+                clear()
+                return None
+        except ValueError:
+            clear()
+            search_choice = None
+            search_entries_menu()
+            print("Please enter a number value! (Ex: 1, 2, 3, 4, 5): ")
+    return search_choice
+    
+            
+def search_by_date(user_input):
+    result_set = date_query(user_input)
+    return result_set
+            
+    
+def all_dates():
+    dates = Database.select().order_by(Database.date.desc())
+    dates_list = []
+    for single_date in dates:
+        format = single_date.date.strftime('%Y-%m-%d')
+        if format not in dates_list:
+            dates_list.append(format)
+    return dates_list
+                           
+                           
+def date_query(user_input):
+    database = Database.select().order_by(Database.date.desc())
+    return database.where(Database.date==user_input)
+        
+        
+def search_by_time(user_input):
+    result_set = time_query(user_input)
+    return result_set
+        
+    
+def time_query(user_input):
+    database = Database.select().order_by(Database.date.desc())
+    return database.where(Database.time == user_input)
+    
+        
+def search_by_exact(user_input):
+    result_set = exact_query(user_input)
+    return result_set
+    
+    
+def exact_query(user_input):
+    return Database.select().where(
+        (Database.name.contains(user_input)) |
+        (Database.notes.contains(user_input)) |
+        (Database.employee_field.contains(user_input)))
+        
+        
+def search_by_employee(user_input):
+    result_set = employee_query(user_input)
+    return result_set
+            
+        
+def all_employees():
+    employee_query = Database.select().order_by(Database.employee_field.asc())
+    employees = []
+    for employee in employee_query:
+        if employee.employee_field not in employees:
+            employees.append(employee.employee_field)
+    return employees
+
+        
+def employee_query(user_input):
+    employees = Database.select().order_by(Database.employee_field.asc())
+    return employees.where(Database.employee_field==user_input)
+    
+                        
+def display_results(result):        
+# initialize a count for how many matches there are
+    clear()
+    if result:
+        count = 0
+        print("Here are your results!\n")
+        # for loop to give each match it's own set of information
+        for match in result:
+            print("Employee Name: {}\n"
+                  "Task Name: {}\n"
+                  "Task Time: {}\n"
+                  "Task Notes: {}\n"
+                  "Task Date: {}\n\n".format(match.employee_field, match.name, match.time, match.notes, match.date)
+                  )
+            # increment the count
+            count += 1
+        # display the total number of matches using the count variable
+        print("Total amount of matches: {}\n".format(count))
+        # return back to the search option menu        
+    else:
+        clear()
+        print("No results!")    
+    return None
+            
+# Verified with Chris I could opt out of testing this.        
+if __name__ == "__main__": # pragma: no cover
+    print(intro_message)
+    create_database()
+    main()
+    
